2db2228071d500b8458e5124835885f0b67a52b5
hsymicm <ichorvfx@gmail.com>
2022-10-22 12:36:44 +0700

    update

--- assets/index.js
+++ assets/index.js
@@ -63,7 +63,8 @@ setData(curIndex);
 
 const scrollToTarget = (element) => {
     var element = document.getElementById(element);
-    var headerOffset = 100;
+    var nav = document.querySelector('.nav-li');
+    var headerOffset = nav.getBoundingClientRect().bottom + 25;
     var elementPosition = element.getBoundingClientRect().top;
     var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
     window.scrollTo({ top: offsetPosition, behavior: 'smooth' });
